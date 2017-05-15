79cff23d40f305540cee35b350c57a4c2d9a3b99
Trina <trina@Trinas-MacBook-Pro.local>
2017-05-15 01:43:13 -0700

    intial commit

---
+++ app/tests/components/Countdown.test.jsx
@@ -0,0 +1,63 @@
+var expect = require('expect');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var $ = require('jQuery');
+
+var Countdown = require('Countdown');
+
+describe('Countdown', () => {
+  it('Countdown should exist', () => {
+     expect(Countdown).toExist();
+  });
+
+  describe('handleSetCountDown', () => {
+    it('should set state to started and countdown', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountDown(10);
+
+      expect(countdown.state.count).toBe(10);
+      expect(countdown.state.countdownStatus).toBe('started');
+
+      setTimeout(() => {
+          expect(countdown.state.count).toBe(9);
+          done();
+      },1001)
+    });
+
+    it('should never set count less than zero', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountDown(1);
+
+      setTimeout(() => {
+          expect(countdown.state.count).toBe(0);
+          done();
+      },3001)
+    });
+
+    it('should pause countdown on paused status', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountDown(3);
+      countdown.handleStatusChange('paused');
+
+      setTimeout(() => {
+          expect(countdown.state.count).toBe(3);
+          expect(countdown.state.countdownStatus).toBe('paused');
+          done();
+      },1001)
+    });
+
+    it('should reset countdown on stopped status', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountDown(1);
+      countdown.handleStatusChange('stopped');
+
+      setTimeout(() => {
+          expect(countdown.state.count).toBe(0);
+          expect(countdown.state.countdownStatus).toBe('stopped');
+          done();
+      },1001)
+    });
+
+  })
+});
